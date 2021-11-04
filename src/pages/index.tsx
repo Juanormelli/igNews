f65e3dac19fa7b50960205389b4de1761f1045f9
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-11-04 09:08:30 -0300

    Primeira chamada API Static

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 import React from "react"
 import { SubscribeButton } from "../components/SubscribeButton"
 import styles from "./home.module.scss"
-import {GetServerSideProps} from"next"
+import {GetStaticProps} from"next"
 import {stripe} from "../services/stripe"
 
 
@@ -46,7 +46,7 @@ export default function Home({product}:HomeProps) {
   )
 }
 
-export const getServerSideProps : GetServerSideProps = async()=>{
+export const getStaticProps : GetStaticProps = async()=>{
   const price = await stripe.prices.retrieve('price_1Jrpe8BRRRqz0R9hrb0cnxZj')
 
   const product = {
@@ -61,7 +61,10 @@ export const getServerSideProps : GetServerSideProps = async()=>{
   return{
     props:{
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 //24 horas
+    
+    
   }
 
 }
