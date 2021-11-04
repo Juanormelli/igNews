ffed86502c21449c44c7c6eb5e7bbec64c5ca6d1
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-11-04 08:41:51 -0300

    Primeira chamada API SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 
 import Head from "next/head"
 import React from "react"
-import { SigninButton } from "../components/SigninButton"
 import { SubscribeButton } from "../components/SubscribeButton"
 import styles from "./home.module.scss"
-export default function Home() {
+import {GetServerSideProps} from"next"
+import {stripe} from "../services/stripe"
+
+
+interface HomeProps {
+  product: {
+    id: string;
+    amount: number;
+  }
+ 
+}
+
+export default function Home({product}:HomeProps) {
+
+ 
+
+
+
   return (
     <>
       <Head>
@@ -15,12 +31,12 @@ export default function Home() {
       <main className={styles.contentContainer}> 
         <section className={styles.hero}>
           <span>🖐 Hey Welcome!</span>
-          <h1>News about the <span>React</span> World.</h1>
+          <h1>News about<br/> the <span>React</span> World.</h1>
           <p>
             Get access to all the publications <br/>
-            <span>for $9.90/mounth</span>
+            <span>for {product.amount}/month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.id}/>
         </section>
 
         <img src="/images/avatar.svg" alt="girlCoding" />
@@ -28,4 +44,24 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps : GetServerSideProps = async()=>{
+  const price = await stripe.prices.retrieve('price_1Jrpe8BRRRqz0R9hrb0cnxZj')
+
+  const product = {
+    priceId : price.id,
+    amount: new Intl.NumberFormat("en-US",{
+      style:"currency",
+      currency:"USD"
+    }).format(price.unit_amount/100) // é sempre salvo em centavos
+
+  } 
+
+  return{
+    props:{
+      product
+    }
+  }
+
 }
