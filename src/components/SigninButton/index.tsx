a93f6d543ac652d04a89df058cfbf408fd15073e
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-11-13 16:12:12 -0300

    Auth with GitHub ok

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,20 +1,26 @@
 import {FaGithub} from "react-icons/fa"
 import{FiX} from "react-icons/fi"
 import styles from "./style.module.scss"
+import {signIn,signOut, useSession} from "next-auth/client"
 
 export function SigninButton(){
-    const isLoggedIn = true
+    const[session]= useSession()
 
-   return isLoggedIn?(
-        <button className={styles.signinButton}>
+    
+    console.log(session)
+
+   return session?(
+        <button type="button" className={styles.signinButton} onClick={()=>signOut()}>
             <FaGithub color ="#04d361"/>
             Juan Ormelli
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ):  
     (
-        <button className={styles.signinButton}>
-            <FaGithub color ="#eba417"/>
+        <button type="button" className={styles.signinButton} onClick={() => signIn('github')}>
+            <FaGithub color ="#eba417"
+            />
+
             Sign in with github
             
         </button>
